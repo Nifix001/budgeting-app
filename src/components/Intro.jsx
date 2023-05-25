94720976ae19ac56ca64ce0e17526f33951caa0e
Nifix001 <101341614+Nifix001@users.noreply.github.com>
2023-05-26 00:16:09 +0100

    New commit

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,3 +1,8 @@
+import { Form } from "react-router-dom"
+
+// library
+import { UseerPlusIcon } from "@heroicons/react/24/solid"
+
 const Intro = () => {
   return (
     <div className="intro">
@@ -8,6 +13,19 @@ const Intro = () => {
         <p>
             Personal budgeting is the secret to financial freedom. Start your journ today.
         </p>
+        <Form method="post">
+            <input type="text" 
+            name="userName" 
+            required 
+            placeholder="What is your name?"
+            aria-label="Your Name"
+            autoComplete="given-name"
+            />
+            <button type="submit" className="btn btn--dark">
+                <span>Create Account</span>
+                <UseerPlusIcon width={20} />
+            </button>
+        </Form>
       </div>
     </div>
   )
